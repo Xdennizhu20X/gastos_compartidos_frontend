a837e688bd370b8fdd28098ecd286d6bcf4e1bd9
xennis Rodriguez <xennislrj25@gmail.com>
2024-08-18 14:50:57 -0500

    change api link

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000/api', // Ajusta según tu entorno
+  baseURL: 'https://gastos-compartidos.onrender.com/api', // Ajusta según tu entorno
   headers: {
     'Content-Type': 'application/json',
   },
