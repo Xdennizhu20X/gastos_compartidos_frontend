09e11cb719ee3ae75710e2c2ba0ced7b447a8af8
xennis Rodriguez <xennislrj25@gmail.com>
2024-08-19 19:59:02 -0500

    viteconfig

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'https://xdennizhu20x.github.io/gastos_compartidos_frontend'
+  base:'https://xdennizhu20x.github.io/gastos_compartidos_frontend/'
 })
